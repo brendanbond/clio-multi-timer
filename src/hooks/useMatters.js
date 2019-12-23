7482b2e53d2ada3ceb3668c45fd8666358c986b3
brendanbond <brendanbond@gmail.com>
2019-12-22 18:14:47 -0600

    whoops

--- src/hooks/useMatters.js
+++ src/hooks/useMatters.js
@@ -1,28 +1,29 @@
-import { useState, useEffect } from 'react';
-import { useAuth } from './useAuth';
+import { useState, useEffect } from "react";
+import { useAuth } from "./useAuth";
 
 function useMatters() {
   const auth = useAuth();
   const [options, setOptions] = useState([
     {
       value: null,
-      label: 'Log in to Clio to access matters.'
+      label: "Log in to Clio to access matters."
     }
   ]);
 
   useEffect(() => {
     if (auth.authToken) {
       auth.getMatters(auth.authToken).then(res => {
+        console.log(res.data);
         const options = res.data.data.map(option => {
           return {
             value: option.id,
-            label: option.display_number + ' ' + option.description
+            label: option.display_number + " " + option.description
           };
         });
         setOptions(options);
       });
     }
-  }, [auth]);
+  }, [auth.authToken]);
 
   return options;
 }
