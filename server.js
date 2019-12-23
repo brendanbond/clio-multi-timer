f65ae46199780308be2b3808d0f34526a705d7d5
brendanbond <brendanbond@gmail.com>
2019-12-23 12:14:09 -0600

    add json body-parser middleware

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const port = process.env.PORT || 5000;
 require("dotenv").config();
 
 app.use(morgan("dev"));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + "/dist"));
